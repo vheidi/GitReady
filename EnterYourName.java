f7912d4b7049f91fb0196880e19f5041d454aa17
Heidi Vargas <heidivargas@cpp.edu>
2024-09-10 01:22:44 -0700

    Add functionality to EnterYourName.java

--- EnterYourName.java
+++ EnterYourName.java
@@ -6,13 +6,17 @@ public class EnterYourName {
         Scanner scanner = new Scanner(System.in);
         
         // Prompt the user to enter their name
-        System.out.print("Enter your name: ");
+        System.out.println("Enter your full name. . .\n ");
         
-        // Read the user's input (name)
-        String name = scanner.nextLine();
+        // Read the user's input 
+        System.out.println("First Name: ");
+        String firstName = scanner.next();
+
+        System.out.println("Last Name: ");
+        String lastName = scanner.next();
         
         // Print a greeting message
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + firstName + " " + lastName + "!");
         
         // Close the scanner
         scanner.close();
